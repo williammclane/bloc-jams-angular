e18bc79474c5f5fd7fdadb33f86b8d50d25ecff3
williammclane <williamfmclane531@yahoo.com>
2017-05-04 10:37:11 -0400

    assignment 3 done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,6 +15,10 @@
              url: '/album',
              templateUrl: '/templates/album.html'
          });
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
+         });
      }
  
  angular.module('blocJams', ['ui.router']);
