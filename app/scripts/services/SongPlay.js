ff7a999d37834b341b2650220d7e8fe93a9bc19d
williammclane <williamfmclane531@yahoo.com>
2017-05-10 20:32:09 -0400

    Assignment 7 done

--- app/scripts/services/SongPlay.js
+++ app/scripts/services/SongPlay.js
@@ -25,12 +25,21 @@
  
     currentSong = song;
  };
+ /**
+ * @function playSong
+ * @desc Play the song
+ * @param {Object} song
+ */
+    var playSong = function(song) {
+        currentBuzzObject.play();
+        song.playing = true;
+    }
          
+
     SongPlayer.play = function(song) {
              if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
          if (currentBuzzObject.isPaused()) {
              currentBuzzObject.play();
